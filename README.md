95604eac1d652371e8178422a56fc875754925f8
Anoiksis <cambellie@hotmail.com>
2021-02-23 20:30:22 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Vue-FinishedItems-Hype
+# vue-ticketcount-tracker
+A small and simple vue hype app with which you can count your finished tasks.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="item-counter">
+        <h1>Total finished</h1>
+        <div class="count-container">
+          {{counter}}
+        </div>
+        <div class="count-btn">
+          <button class="btn-default" @click="addItem">Item finished</button>
+          <button v-show="countStarted" class="btn-default btn-outline" @click="removeItem">Remove item</button>          
+          <button v-show="countStarted" class="btn-default btn-no-outline" @click="resetCount">Reset counter</button>  
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import VueConfetti from 'vue-confetti';
+
+  Vue.use(VueConfetti);
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      addItem: function() {
+        this.counter += 1;
+        this.startConfetti()
+        setTimeout(() => this.stopConfetti(), 1500);
+        
+      },
+      removeItem: function() {
+        this.counter -= 1;
+      },
+      resetCount: function() {
+        this.counter = 0;
+      },
+      startConfetti() {
+        this.$confetti.start();
+      },
+
+      stopConfetti() {
+        this.$confetti.stop();
+      },
+    },
+    computed: {
+      countStarted: function(){
+        return this.counter > 0;
+      }
+    }
+  }
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  box-sizing: border-box;
+  letter-spacing: 1px;
+}
+
+main {
+  background: rgb(34,193,195);
+  background: linear-gradient(174deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
+  width: 100%;
+  min-height: 100vh;
+  padding: 25px;
+}
+
+.item-counter {
+  background: rgba(255, 255, 255, 0.4);
+  border-radius: 0 16px 0 16px;
+  padding: 25px;
+  width: 300px;
+  margin: 30px auto;
+  text-align: center;
+  box-shadow: 4px 0 8px rgba(0, 0, 0, 0.25);
+}
+
+.item-counter h1 {
+  font-size: 1.4em;
+  font-weight: 500;
+  text-transform: uppercase;
+}
+
+.item-counter .count-container {
+  margin-top: 25px;
+  font-size: 4em;
+  font-weight: 700;
+}
+
+.count-btn {
+  width: 100%;
+  margin-top: 25px
+}
+
+.btn-default {
+  width: 200px;
+  font-size: 0.9em;
+  font-weight: 700;
+  padding: 10px;
+  background: #fdbb2d;
+  border: 2px solid #fdbb2d;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+
+button:focus {
+  outline: none;
+}
+
+.btn-default.btn-outline {
+  background: transparent;
+  border: 2px solid #fdbb2d;
+}
+
+.btn-default.btn-no-outline {
+  border-color: transparent;
+  background: transparent;
+  color: #4e4e4e;
+}
+
+</style>
