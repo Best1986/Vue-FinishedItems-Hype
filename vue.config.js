279a75ea34968f52a05cf70a1b7aad773f427f74
Conny (prive) <37776144+Best1986@users.noreply.github.com>
2021-02-24 12:36:19 +0100

    Deploy cleanup
    hope this works

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/Vue-FinishedItems-Hype/'
-      : '/',
-    pwa: {
-      workboxOptions: {
-        exclude: [/_redirects/]
-      }
-    }
+      : '/'
   }
+  
