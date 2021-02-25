c9bd227d600f19966e9b5fffdcb25419dca50e6d
Anoiksis <cambellie@hotmail.com>
2021-02-25 20:52:31 +0100

    Added window close stopper

--- src/main.js
+++ src/main.js
@@ -6,3 +6,10 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+//Tab close prompt after btn click
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  e.returnValue = 'Have you made a record of your count? Your data will not be saved.';
+});
